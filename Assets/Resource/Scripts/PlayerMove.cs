bffbd8991c6974291a1d5d50fc8046a45c817c82
lKori <yuy0841@gmail.com>
2024-06-22 23:12:24 +0900

    Feat: 점프 게이지 및 캐릭터 이동 삭제
    점프킹처럼 스페이스바 누르는 시간에 비례해서 점프 높이 다르게 구현
    
    좌우키 입력 시 캐릭터 이동하는 기능 삭제

--- Assets/Resource/Scripts/PlayerMove.cs
+++ Assets/Resource/Scripts/PlayerMove.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -11,7 +14,10 @@ public class PlayerMove : MonoBehaviour
     float maxSpeed;
 
     [SerializeField]
+    float minJumpPower;
     float jumpPower;
+    [SerializeField]
+    float jumpTimeLimit;
 
     bool isJumping = false;
 
@@ -29,13 +35,79 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Jump
-        if (Input.GetButtonDown("Jump") && !isJumping)
+        DoJump();
+
+        //MoveStop();
+    }
+
+    private void FixedUpdate()
+    {
+        //DoMove();
+
+        JumpGauge();
+
+        GetLandingPlatform();
+    }
+
+    private void DoMove()
+    {
+        // Move
+        if (!Input.GetKey(KeyCode.Space) && !isJumping)
         {
+            float h = Input.GetAxisRaw("Horizontal");
+
+            rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
+
+            // Max Speed
+            if (rigid.velocity.x > maxSpeed)
+            {
+                rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
+            }
+            else if (rigid.velocity.x < maxSpeed * (-1))
+            {
+                rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
+            }
+        }
+    }
+
+    private void DoJump()
+    {
+        // Jump
+        if (Input.GetButtonUp("Jump") && !isJumping)
+        {
+            if(jumpPower < minJumpPower)
+            {
+                jumpPower = minJumpPower;
+            }
+
             isJumping = true;
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-        }
+            rigid.velocity = new Vector2(maxSpeed * Input.GetAxisRaw("Horizontal"), rigid.velocity.y);
 
+            jumpPower = 0;
+        }
+    }
+
+    private void JumpGauge()
+    {
+        if(Input.GetKey(KeyCode.Space) && !isJumping)
+        {
+            if(jumpTimeLimit > jumpPower)
+            {
+                jumpPower += 0.3f;
+            }
+
+            if(jumpTimeLimit < jumpPower)
+            {
+                jumpPower = jumpTimeLimit;
+            }
+
+            Debug.Log(jumpPower);
+        }
+    }
+
+    private void MoveStop()
+    {
         // Move Stop
         if (Input.GetButtonUp("Horizontal") && !isJumping)
         {
@@ -43,40 +115,21 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
+    private void GetLandingPlatform()
     {
-        // Move
-        if(!isJumping)
-        {
-            float h = Input.GetAxisRaw("Horizontal");
-
-            rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-
-            // Max Speed
-            if(rigid.velocity.x > maxSpeed)
-            {
-                rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-            }
-            else if(rigid.velocity.x < maxSpeed * (-1))
-            {
-                rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
-            }
-        }
-
-
         // Landing Platform
         Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
 
         RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
 
-        if(rigid.velocity.y < 0)
+        if (rigid.velocity.y < 0)
         {
-            if(rayHit.collider != null)
+            if (rayHit.collider != null)
             {
-                if(rayHit.distance < 0.9f)
+                if (rayHit.distance < 0.85f)
                 {
-                    Debug.Log(rayHit.collider.name);
                     isJumping = false;
+                    rigid.velocity = new Vector2(0, 0);
                 }
             }
         }
