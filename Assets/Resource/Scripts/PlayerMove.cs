4e746fac004a059b5da757e6e79009bb3797fe00
lKori <yuy0841@gmail.com>
2024-07-02 00:00:03 +0900

    Feat: 캐릭터 높이에 따라 카메라 이동 Feat: 캐릭터 현재 상태 가져오기 함수 추가

--- Assets/Resource/Scripts/PlayerMove.cs
+++ Assets/Resource/Scripts/PlayerMove.cs
@@ -11,22 +11,37 @@ public class PlayerMove : MonoBehaviour
     Rigidbody2D rigid;
 
     [SerializeReference]
-    float maxSpeed;
+    float maxSpeed; // 좌우 이동 최고속도
 
     [SerializeField]
-    float minJumpPower;
-    float jumpPower;
+    float minJumpPower; // 최소 점프 높이
+    float jumpPower;    // 점프 충전 게이지
     [SerializeField]
-    float jumpSpeedX;
+    float jumpSpeedX;   // 점프 중 좌우 이동 속도
     [SerializeField]
-    float jumpTimeLimit;
+    float jumpTimeLimit;    // 점프 게이지 최대 시간
 
-    bool isJumping = false;
+    bool isJumping = false; // 점프 상태
+
+    float playerPrevY;
+
+    public enum PlayerStatus
+    {
+        Idle,   // 기본
+        WalkLeft,   // 왼쪽으로 걷기
+        WalkRight,  // 오른쪽으로 걷기
+        Jump,   // 점프 중
+        Sit,    // 점프 게이지 충전
+        Fall    // 떨어지는 중
+    }
+
+    PlayerStatus playerStatus;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerStatus = PlayerStatus.Idle;
+        playerPrevY = this.transform.position.y;
     }
 
     private void Awake()
@@ -37,82 +52,109 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 캐릭터 점프
         DoJump();
 
+        // 캐릭터 정지
         MoveStop();
+
+        // 떨어지는 중인지 체크
+        CheckPlayerFall();
     }
 
     private void FixedUpdate()
     {
+        // 캐릭터 이동
         DoMove();
 
+        // 점프 충전
         JumpGauge();
 
+        // 벽에 부딪혔을 때 튕기기
+        BouncePlayer();
+
+        // 땅 위에 있는지 판별
         GetLandingPlatform();
+
+        Debug.Log($"Player Status: {playerStatus}");
     }
 
     private void DoMove()
     {
-        // Move
+        // 점프 상태가 아니며 점프 키 입력 중이 아닐 때 이동 가능
         if (!Input.GetKey(KeyCode.Space) && !isJumping)
         {
+            // 입력 방향이 왼쪽인지 오른쪽인지
             float h = Input.GetAxisRaw("Horizontal");
 
+            // 플레이어 상태 변경
+            if (h == 1)
+            {
+                playerStatus = PlayerStatus.WalkRight;
+            }
+            else if(h == -1)
+            {
+                playerStatus = PlayerStatus.WalkLeft;
+            }
+
+            // 입력 방향으로 이동
             rigid.velocity = new Vector2(maxSpeed * h, rigid.velocity.y);
-
-            //rigid.AddForce(Vector2.right * h, ForceMode2D.Impulse);
-
-            //// Max Speed
-            //if (rigid.velocity.x > maxSpeed)
-            //{
-            //    rigid.velocity = new Vector2(maxSpeed, rigid.velocity.y);
-            //}
-            //else if (rigid.velocity.x < maxSpeed * (-1))
-            //{
-            //    rigid.velocity = new Vector2(maxSpeed * (-1), rigid.velocity.y);
-            //}
         }
     }
 
     private void MoveStop()
     {
-        // Move Stop
-        if (Input.GetButtonUp("Horizontal") && !isJumping)
+        // 점프 상태가 아니며 좌우 키가 입력이 되지 않을 때 정지
+        if (!Input.GetButtonUp("Horizontal") && !isJumping)
         {
+            // 플레이어 상태 변경
+            playerStatus = PlayerStatus.Idle;
+
             rigid.velocity = new Vector2(0, rigid.velocity.y);
         }
     }
 
     private void DoJump()
     {
-        // Jump
+        // 점프 상태가 아니며 점프 키를 뗐을 때 점프 실행
         if (Input.GetButtonUp("Jump") && !isJumping)
         {
-            if(jumpPower < minJumpPower)
+            // 플레이어 상태 변경
+            playerStatus = PlayerStatus.Jump;
+
+            // 점프 높이가 최소 점프 높이보다 낮으면 최소 점프 높이로 점프 실행
+            if (jumpPower < minJumpPower)
             {
                 jumpPower = minJumpPower;
             }
 
+            // 점프 실행
             isJumping = true;
             rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
             rigid.velocity = new Vector2(jumpSpeedX * Input.GetAxisRaw("Horizontal"), rigid.velocity.y);
 
+            // 점프 충전 게이지 초기화
             jumpPower = 0;
         }
     }
 
     private void JumpGauge()
     {
-        if(Input.GetKey(KeyCode.Space) && !isJumping)
+        // 점프 중이 아니며 점프 키를 계속 누르고 있을 경우 점프 게이지 충전
+        if (Input.GetKey(KeyCode.Space) && !isJumping)
         {
+            playerStatus = PlayerStatus.Sit;
+
+            // 점프 게이지 충전 중에는 이동 중지
             rigid.velocity = new Vector2(0, 0);
 
-            if(jumpTimeLimit > jumpPower)
+            if (jumpTimeLimit > jumpPower)
             {
-                jumpPower += 0.3f;
+                jumpPower += 0.5f;
             }
 
-            if(jumpTimeLimit < jumpPower)
+            // 최대 점프 높이보다 낮을 경우 게이지 충전
+            if (jumpTimeLimit < jumpPower)
             {
                 jumpPower = jumpTimeLimit;
             }
@@ -121,23 +163,64 @@ public class PlayerMove : MonoBehaviour
         }
     }
 
+    private void BouncePlayer()
+    {
+        Debug.DrawRay(rigid.position, Vector3.right, new Color(1, 0, 0));
+        Debug.DrawRay(rigid.position, Vector3.left, new Color(-1, 0, 0));
+
+        float rayCastLen = 0.33f;   // Raycast 길이
+        float checkDistance = 0.33f;    // Raycast 감지 거리
+
+        RaycastHit2D rayHitRight = Physics2D.Raycast(rigid.position, Vector3.right, rayCastLen, LayerMask.GetMask("Platform")); // 오른쪽 벽 충돌 감지
+        RaycastHit2D rayHitLeft = Physics2D.Raycast(rigid.position, Vector3.left, rayCastLen, LayerMask.GetMask("Platform"));   // 왼쪽 벽 충돌 감지
+
+        // 캐릭터의 왼쪽 또는 오른쪽에서 벽이 감지 되었을 경우
+        if (rayHitRight.collider != null || rayHitLeft.collider != null)
+        {
+            // 캐릭터와 벽 충돌 시 반대 방향으로 튕기기
+            if (rayHitRight.distance < checkDistance || rayHitLeft.distance < checkDistance)
+            {
+                rigid.velocity = new Vector2(-rigid.velocity.x, rigid.velocity.y);
+            }
+        }
+    }
+
     private void GetLandingPlatform()
     {
-        // Landing Platform
-        Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 1, 0));
+        Debug.DrawRay(rigid.position, Vector3.down, new Color(0, 0.52f, 0));
 
-        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+        RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));    // 캐릭터 아래쪽으로 바닥 감지
 
         if (rigid.velocity.y < 0)
         {
             if (rayHit.collider != null)
             {
-                if (rayHit.distance < 0.85f)
+                if (rayHit.distance < 0.52f)
                 {
+                    // 플레이어 상태 변경
+                    playerStatus = PlayerStatus.Idle;
+
                     isJumping = false;
                     rigid.velocity = new Vector2(0, 0);
                 }
             }
         }
+    }
+
+    private void CheckPlayerFall()
+    {
+        float playerNowY = this.transform.position.y;
+
+        if (playerNowY < playerPrevY)
+        {
+            playerStatus = PlayerStatus.Fall;
+        }
+
+        playerPrevY = playerNowY;
+    }
+
+    public PlayerStatus GetPlayerStatus()
+    {
+        return playerStatus;
     }
 }
